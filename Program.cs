5c2665b7a0e837091f3a567366023dbeb6d9d380
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-08-15 20:22:30 +0200

    DAMN DAT MOVEMENT THOUGH

--- Program.cs
+++ Program.cs
@@ -8,38 +8,45 @@ namespace Raylib_Test
   {
     static void Main(string[] args)
     {
-      Raylib.ToggleFullscreen();
-      Raylib.InitWindow(800, 600, "Hello World");
+      int width = 1200;
+      int height = 600;
+
+      int gameWidth = width - 50;
+      int gameHeight = height - 50;
+
+      Raylib.InitWindow(width, height, "Hello World");
+
+      Rectangle playerRect = new Rectangle(150, 150, 50, 50);
 
       Color newPink = new Color(255, 105, 180, 255);
-      Color white = Color.WHITE;
+
       Raylib.SetExitKey(KeyboardKey.KEY_ESCAPE);
-      Rectangle playerRect = new Rectangle(150, 150, 50, 50);
+
 
       while (!Raylib.WindowShouldClose())
       {
         Vector2 mousePos = Raylib.GetMousePosition();
         bool areOverlapping = Raylib.CheckCollisionPointRec(mousePos, playerRect);
 
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && playerRect.y != 550)
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && playerRect.y != gameHeight)
         {
-          playerRect.y += Convert.ToSingle(0.5);
+          playerRect.y += Convert.ToSingle(2);
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && playerRect.y != 0)
         {
-          playerRect.y -= Convert.ToSingle(0.5);
+          playerRect.y -= Convert.ToSingle(2);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && playerRect.x != 750)
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && playerRect.x != gameWidth)
         {
-          playerRect.x += Convert.ToSingle(0.5);
+          playerRect.x += Convert.ToSingle(2);
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_A) && playerRect.x != 0)
         {
-          playerRect.x -= Convert.ToSingle(0.5);
+          playerRect.x -= Convert.ToSingle(2);
         }
 
         Raylib.BeginDrawing();
-        Raylib.ClearBackground(white);
+        Raylib.ClearBackground(Color.WHITE);
 
         Raylib.DrawText(mousePos.ToString(), 10, 10, 25, Color.ORANGE);
 
@@ -51,6 +58,7 @@ namespace Raylib_Test
         Raylib.DrawRectangleRec(playerRect, Color.SKYBLUE);
 
         Raylib.EndDrawing();
+
       }
     }
   }
